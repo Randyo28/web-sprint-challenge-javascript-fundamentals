242bcbf1875c3c8d557d54d303009ded6c11246b
Randy Ortiz <Randyortiz28@gmail.com>
2021-01-29 11:25:26 -0500

    Answered first task

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+//The Nested function can access the variable internal because it is a closure. Nested Function doesn't have it so it looks up to the outer scope into myFunction and grabs internal variable.
 
 
 
@@ -28,11 +28,12 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+  for(let i = 0; i < num.length; i++){
+    return num[i]
   }
- 
+  }
+ console.log
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
